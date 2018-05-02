6b5efb8fa9308870cdc6c0000ceb0bdb3c202d19
Alex <alexlev0308@mail.ru>
2018-05-02 22:50:41 +0300

    Log Grizzly

--- src/main/java/moneytransfer/Start.java
+++ src/main/java/moneytransfer/Start.java
@@ -2,18 +2,27 @@ package moneytransfer;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.URI;
 
-
 public class Start {
     public static void main(String[] args) throws IOException {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+        Logger.getLogger("").setLevel(Level.FINEST);
+
         URI baseUri = UriBuilder.fromUri("http://localhost/").port(90).build();
         ApplicationConfig config = new ApplicationConfig();
 
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+
 
         System.out.println("Press enter to stop the server...");
         System.in.read();
